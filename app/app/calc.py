f49f5b776ba51e3ded2fa511b8591530f039d08c
Arbinda Kumar Prasad <arbinda.prasad@juspay.in>
2023-12-02 16:37:40 +0530

    s7-v29: Write a test using TDD

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator function
 def add(x, y): 
     """Add x and y and return result"""
     return x + y
+
+def subtract(x, y):
+    """Subtract x and y and return result"""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -11,3 +11,8 @@ class CalcTest(SimpleTestCase):
         """Add numbers"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Subtract numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
